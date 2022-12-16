cf8d8310798db9e46bfdfa8d217bcf9b27ca3093
Ketsuppimakkara <77271158+Ketsuppimakkara@users.noreply.github.com>
2022-12-16 14:40:24 +0200

    added vehicle/add route

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.json());
+let vehicleList = [];
 app.get("/", (req, res) => {
     res.send("Hello from TS-express");
 });
 app.get("/hello", (req, res) => {
     res.send("Hello world");
+});
+app.post("/vehicle/add", (req, res) => {
+    if (!req.body.model || !req.body.color || !req.body.year || !req.body.power) {
+        res.status(400).send("Malformed request");
+    }
+    else {
+        const newVehicle = {
+            model: req.body.model,
+            color: req.body.color,
+            year: req.body.year,
+            power: req.body.power
+        };
+        vehicleList.push(newVehicle);
+        console.log(vehicleList);
+        res.status(201).send("Vehicle added");
+    }
 });
 app.listen(port, () => {
     console.log("Server is up and running");
--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import express, {Express, Request, Response} from "express"
-
 const app: Express = express()
-const port: number = 3000
+const port: Number = 3000
+app.use(express.json())
+
+interface Vehicle {
+    model: String;
+    color: String;
+    year: Number;
+    power: Number;
+}
+
+let vehicleList: Vehicle[] = []
 
 app.get("/",(req: Request,res: Response) =>{
     res.send("Hello from TS-express")
@@ -9,6 +18,23 @@ app.get("/",(req: Request,res: Response) =>{
 
 app.get("/hello",(req: Request,res: Response) =>{
     res.send("Hello world")
+})
+
+app.post("/vehicle/add",(req: Request,res: Response) =>{
+    if(!req.body.model || !req.body.color || !req.body.year|| !req.body.power){
+        res.status(400).send("Malformed request")
+    }
+    else{
+        const newVehicle: Vehicle = {
+            model:req.body.model,
+            color:req.body.color,
+            year:req.body.year,
+            power:req.body.power
+        }
+        vehicleList.push(newVehicle)
+        console.log(vehicleList)
+    res.status(201).send("Vehicle added")
+    }
 })
 
 app.listen(port, () =>{
